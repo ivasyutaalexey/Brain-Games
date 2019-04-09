e5eed707f36f72c719fb9b22c3a5ea34e5a119c7
a_ivasyuta <ivasyutaalexey@gmail.com>
2019-04-09 20:05:30 +0300

    added game logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import readlineSync from 'readline-sync';
 
+
+const getRandomInt = () => Math.floor(Math.random() * (100 - 1)) + 1;
+
 export default () => {
-  const name = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  let gameCount = 0;
+  let rightAnswer = true;
+
+  while (gameCount < 3 && rightAnswer) {
+    const digit = getRandomInt();
+    const correctAnswer = digit % 2 === 0 ? 'yes' : 'no';
+
+    console.log(`Question: ${digit}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer === answer) {
+      gameCount += 1;
+      console.log('Correct!');
+    } else {
+      rightAnswer = false;
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    }
+  }
+
+  if (gameCount === 3 && rightAnswer) {
+    console.log(`Congratulations, ${name}!`);
+  }
 };
